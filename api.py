39d063697b5e4d9a91fd6b3523b7f3db32c8e762
izaansohail <k180162@nu.edu.pk>
2020-12-09 03:17:03 +0500

    all done except match face api

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
-
+import sys
+import base64
+import cv2
+import os
 import flask
-from flask import request
+from flask import request, send_from_directory, send_file
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 import json
@@ -14,32 +17,55 @@ class JSONEncoder(json.JSONEncoder):
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-#app.config['MONGO_DBNAME'] = 'Citizen'
-#app.config["MONGO_URI"] = "mongodb+srv://usama:usama@izaan.0y3eq.mongodb.net/Citizen?retryWrites=true&w=majority"
 client = MongoClient(
     "mongodb+srv://usama:usama@izaan.0y3eq.mongodb.net/Izaan?retryWrites=true&w=majority")
 
 
-@app.route('/citizen', methods=['POST' , "PUT" , "GET"] )
+@app.route('/citizen', methods=['POST' , "PUT" , "GET", "DELETE"] )
 def citizen():
     if request.method == "POST":
+        print("here")
         data = request.get_json()
+        try:
+            # creating a folder named data
+            if not os.path.exists('data'):
+                os.makedirs('data')
+            # if not created then raise error
+        except OSError:
+            print('Error: Creating directory of data')
+
         #print(data)
         db = client['Citizen']
         citizenData = db.CitizenData
+        cnic = request.get_json()["cnic"]
+        f_image1 = open("./data/"+cnic+"-front.jpg", "wb")
+        b64str = request.get_json()['image1_name']
+        f_image1.write(base64.b64decode(b64str))
+        f_image1.close()
+        data["image1_name"] = "/data/"+cnic+"-front.jpg"
+
+        f_image2 = open("./data/"+cnic+"-right.jpg", "wb")
+        b64str = request.get_json()['image2_name']
+        f_image2.write(base64.b64decode(b64str))
+        f_image2.close()
+        data["image2_name"] = "/data/"+cnic+"-right.jpg"
+
+        f_image3 = open("./data/"+cnic+"-left.jpg", "wb")
+        b64str = request.get_json()['image3_name']
+        f_image3.write(base64.b64decode(b64str))
+        f_image3.close()
+        data["image3_name"] = "/data/"+cnic+"-left.jpg"
         citizenid = citizenData.insert_one(data)
         return {"citizenId" :str(citizenid.inserted_id) }
     
     if request.method == "GET":
+        cnic = request.args.get('cnic')
+        print(cnic)
         db = client['Citizen']
         citizenData = db.CitizenData
-        citizens = citizenData.find()
-        res=[]
-        for citizen in citizens:
-            print(citizen)
-            citizen["_id"] = str(citizen["_id"])
-            res.append(citizen)
-        obj= {"citizens": res}
+        reqcitizen = citizenData.find_one({"cnic": cnic})
+        reqcitizen["_id"] = str(reqcitizen["_id"])
+        obj= {"citizen": reqcitizen}
         print(type(obj), obj)
         return obj
     
@@ -59,7 +85,25 @@ def citizen():
             else :
                 return {"msg" : "record cannot be updated"}
 
+    if request.method == "DELETE":
+        data = request.json
+        cnic = request.get_json()["cnic"]
+        print(cnic)
+        db = client['Citizen']
+        citizenData = db.CitizenData
+        reqcitizen = citizenData.find_one({"cnic": cnic})
+        if reqcitizen == None:
+            return {"error": "could not find the cnic"}
+        else:
+            citizenid = citizenData.delete_one(reqcitizen)
+            if  citizenid != None:
+                return {"msg":  "record Deleted"}
+            else:
+                return {"msg": "record cannot be deleted"}
 
-# D: \izaan\Work\University\university docs\Semester 5\Database Systems\Project\BackEnd
+
+@app.route('/data/<path:path>')
+def send_js(path):
+    return send_file('./data/'+path, mimetype="image/jpg")
 
 app.run(host="0.0.0.0")
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+ print(frame1)
